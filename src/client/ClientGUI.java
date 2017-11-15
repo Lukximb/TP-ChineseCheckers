b9242e75f43163dfe6dcfc3a7a54ab015905da4c
Piotr Berezowski <piotr.berezowski97@gmail.com>
2017-11-15 19:54:37 +0100

    Client GUI

---
+++ src/client/ClientGUI.java
@@ -0,0 +1,77 @@
+package client;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.rmi.RemoteException;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+
+@SuppressWarnings("serial")
+public class ClientGUI extends JFrame implements ActionListener {
+	Client client;
+	JButton submitButton;
+	JTextField inputField;
+	JLabel outputField;
+	
+	public ClientGUI(Client client) {
+		super("Chinese Checkers");
+		this.client = client;
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
+		setSize(600, 600);
+		
+		JPanel p1 = new JPanel();
+		p1.setLayout(new BoxLayout(p1, BoxLayout.X_AXIS));
+		JPanel p2 = new JPanel();
+		
+		inputField = new JTextField();
+		inputField.setPreferredSize(new Dimension(200, 20));
+		submitButton = new JButton("Send");
+		submitButton.addActionListener(this);
+		
+		outputField = new JLabel();
+		
+		p1.add(inputField);
+		p1.add(submitButton);
+		p2.add(outputField);
+		this.add(p1);
+		this.add(p2);
+		
+		setVisible(true);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object source = e.getSource();
+		if(source == submitButton) {
+				try {
+					client.p.printStr(sendString());
+				} catch (RemoteException e1) {
+					e1.printStackTrace();
+				}
+		}
+	}
+	
+	String sendString() {
+		String s = inputField.getText();
+		return s;
+	}
+	
+//	public static void main(String[] args) {
+//		EventQueue.invokeLater(new Runnable() {
+//			@Override
+//			public void run() {
+//				new ClientGUI();
+//			}
+//		});
+//	}
+}
