cd9742330f6981889f6884003674ff25d7408e8a
Piotr Berezowski <piotr.berezowski97@gmail.com>
2017-11-18 07:30:15 +0100

    add GUI in javafx

---
+++ src/client/ClientGUIController.java
@@ -0,0 +1,202 @@
+package client;
+
+import java.rmi.RemoteException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+
+public class ClientGUIController {
+	ClientGUI client;
+
+	//TEST---------------------------------------
+	@FXML
+	private StackPane test;
+	@FXML
+	private Button submitButton;
+	@FXML
+	private Label outputLabel;
+	@FXML
+	private TextField inputField;
+
+	//MENU----------------------------------------
+	@FXML
+	private StackPane menu;
+	@FXML
+	private TextField nickNameField;
+	@FXML
+	private Button newGameButton;
+	@FXML
+	private Button exitButton;
+	@FXML
+	private Button joinGameButton;
+	//CREATE LOBBY--------------------------------
+	@FXML
+	private StackPane createLobby;
+	@FXML
+	private TextField lobbyNameField;
+	@FXML
+	private Button player2Button;
+	@FXML
+	private Button player4Button;
+	@FXML
+	private Button player6Button;
+	@FXML
+	private Button createLobbyButton;
+	@FXML
+	private Button cancelCreateLobbyButton;
+	//JOIN LOBBY----------------------------------
+	@FXML
+	private StackPane joinLobby;
+	@FXML
+	private Button cancelJoinLobbyButton;
+	@FXML
+	private Button joinLobbyButton;
+	@FXML 
+	private TableView<Integer> lobbyTable;
+	@FXML
+	private TableColumn<Integer, String> lobbyNameColumn;
+	@FXML
+	private TableColumn<Integer, Integer> pawnColumn;
+	@FXML 
+	private TableColumn<Integer, Integer> playerColumn;	
+	//LOBBY---------------------------------------
+	@FXML
+	private StackPane lobby;
+	@FXML
+	private TextField invitePlayerField;
+	@FXML
+	private Button addBotButton;
+	@FXML
+	private Button removePlayerButton;
+	@FXML
+	private Button easyBotButton;
+	@FXML
+	private Button mediumBotButton;
+	@FXML
+	private Button hardBotButton;
+	@FXML
+	private Button readyButton;
+	@FXML
+	private Button exitLobbyButton;
+	//GAME----------------------------------------
+	@FXML
+	private StackPane game;
+	@FXML
+	private ProgressBar turnTimeBar;
+	@FXML
+	private Button moveButton;
+	@FXML
+	private Button passButton;
+	@FXML
+	private Button surrenderButton;
+	@FXML
+	private Button sendMsgButton;
+	
+	
+	ClientGUIController(ClientGUI client) {
+		this.client = client;
+	}
+	
+	@FXML
+	void initialize() {
+	}
+	
+	//MENU
+	public void newGameButtonOnClick(ActionEvent exent) {
+		this.menu.setVisible(false);
+		this.menu.setDisable(true);
+		
+		this.createLobby.setVisible(true);
+		this.createLobby.setDisable(false);
+	}
+	
+	public void exitButtonOnClick(ActionEvent event) {
+		
+	}
+	
+	public void joinGameButtonOnClick(ActionEvent event) {
+		
+	}
+	//CREATE LOBBY
+	public void createLobbyButtonOnClick(ActionEvent event) {		
+		this.createLobby.setVisible(false);
+		this.createLobby.setDisable(true);
+		
+		this.joinLobby.setVisible(true);
+		this.joinLobby.setDisable(false);
+	}
+	
+	public void cancelCreateLobbyButtonOnClick(ActionEvent event) {
+		this.createLobby.setVisible(false);
+		this.createLobby.setDisable(true);
+		
+		this.menu.setVisible(true);
+		this.menu.setDisable(false);
+	}
+	
+	//JOIN LOBBY
+	public void joinLobbyButtonOnClick(ActionEvent event) {
+		this.joinLobby.setVisible(false);
+		this.joinLobby.setDisable(true);
+		
+		this.lobby.setVisible(true);
+		this.lobby.setDisable(false);
+	}
+	
+	public void cancelJoinLobbyButtonOnClick(ActionEvent event) {
+		this.joinLobby.setVisible(false);
+		this.joinLobby.setDisable(true);
+		
+		this.createLobby.setVisible(true);
+		this.createLobby.setDisable(false);
+	}
+	
+	//LOBBY
+	public void readyButtonOnClick(ActionEvent event) {
+		this.lobby.setVisible(false);
+		this.lobby.setDisable(true);
+		
+		this.game.setVisible(true);
+		this.game.setDisable(false);
+	}
+	
+	public void exitLobbyButtonOnClick(ActionEvent event) {
+		this.lobby.setVisible(false);
+		this.lobby.setDisable(true);
+		
+		this.menu.setVisible(true);
+		this.menu.setDisable(false);
+	}
+	
+	//GAME
+	public void surrenderButtonOnClick(ActionEvent event) {
+		this.game.setVisible(false);
+		this.game.setDisable(true);
+		
+		this.menu.setVisible(true);
+		this.menu.setDisable(false);
+	}
+	
+	//TEST
+	public void submitButtonOnClick(ActionEvent event) {
+		try {
+			client.p.printStr(this.sendString());
+			this.outputLabel.setText(client.p.getReply());
+		} catch (RemoteException e1) {
+			e1.printStackTrace();
+		}
+	}
+	
+	private String sendString() {
+		String s = inputField.getText();
+		return s;
+	}
+	
+}
