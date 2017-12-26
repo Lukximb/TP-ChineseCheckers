0a605994c0a531a13257f90f6250db0be3dea422
Wojtek Zaniewski <wojtek.z@interia.eu>
2017-12-26 20:48:32 +0100

    Added example generator for NN with 234 generated examples

---
+++ botNN/src/generator/GeneratorLauncher.java
@@ -0,0 +1,41 @@
+package generator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ * Simple application to generating data sample for neural network
+ * When app run, array is filling by 1, next we click on field which neural network should choose
+ * 1 - player on field, we cannot choose this field
+ * 0 - empty field, we can choose it
+ */
+public class GeneratorLauncher extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(this.getClass().getResource("/generator/dataBoardGenerator.fxml"));
+
+        Controller controller = new Controller();
+        loader.setController(controller);
+        StackPane stackPane=loader.load();
+
+        Scene scene = new Scene(stackPane);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Bot data generator");
+
+        primaryStage.show();
+    }
+
+    @Override
+    public void init() {
+    }
+}
